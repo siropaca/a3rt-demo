3822ead0e7034d824915c81fe290a4078f6eb0f1
shirai ryohei <shirai.ryohei0302@gmail.com>
2020-02-21 23:13:34 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const config = require("./.config.json");
+const request = require("request-promise");
+
+let options = {
+  method: "POST",
+  uri: "https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk",
+  timeout: 3000,
+  json: true,
+  form: {
+    apikey: config.TALK_API_KEY,
+    query: "おはよう"
+  }
+};
+
+request(options)
+  .then(body => {
+    console.log(body);
+  })
+  .catch(err => {
+    console.log(err);
+  });
